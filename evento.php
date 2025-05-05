36a4cf2a49fb8291d92d18315b7d881a097895c9
Miguel Angel <miguelangel_medina@yahoo.com.ar>
2025-05-05 07:59:22 -0300

    T-15: Form Inscripción de RN (feat)

--- evento.php
+++ evento.php
@@ -6,6 +6,8 @@
   $arr_  = array();
   $arr_  = $Eve->gets($id); 
   $titulo= $arr_[0]['titulo'];
+  $tipo  = $arr_[0]['tipo'];
+  $_SESSION['ses_id_evento']= $id;
 
 ?>
 
@@ -78,6 +80,10 @@ box-shadow: 5px 5px 20px 0 rgba(0,0,0,0.4) !important
         </div>
       </div>
     </section>
+
+
+
+
     <section class="u-clearfix u-grey-10 u-section-3" id="sec-9946">
       <div class="u-clearfix u-sheet u-valign-middle-lg u-valign-middle-md u-valign-middle-sm u-valign-middle-xs u-sheet-1">
         <div class="u-container-style u-expanded-width u-group u-radius u-shape-round u-white u-group-1">
@@ -87,91 +93,31 @@ box-shadow: 5px 5px 20px 0 rgba(0,0,0,0.4) !important
                 <h1 class="u-custom-font u-font-montserrat u-text u-text-white u-text-1">Formulario de inscripción</h1>
               </div>
             </div>
+
+
             <div class="u-container-style u-group u-white u-group-3">
               <div class="u-container-layout u-container-layout-3">
-                <div class="u-form u-form-1">
-                  <form action="https://forms.nicepagesrv.com/v2/form/process" class="u-clearfix u-form-spacing-28 u-form-vertical u-inner-form" source="email" name="form" style="padding: 0px;">
-                    <div class="u-form-checkbox-group u-form-group u-form-group-1">
-                      <label class="u-label">Productos a comercializar:</label>
-                      <div class="u-form-checkbox-group-wrapper">
-                        <div class="u-input-row">
-                          <input id="field-c404" type="checkbox" name="checkbox-1[]" value="Item 1 lllllllllllllll kkkkkkkkkkkkkkkkkkk nnnnnnnnnnnnnn" class="u-field-input" checked="checked" data-calc="">
-                          <label class="u-field-label" for="field-c404">Item 1 lllllllllllllll kkkkkkkkkkkkkkkkkkk nnnnnnnnnnnnnn</label>
-                        </div>
-                        <div class="u-input-row">
-                          <input id="field-d13d" type="checkbox" name="checkbox-1[]" value="Item 2 ffffffffff ddddddddddddddddddddddddd" class="u-field-input" data-calc="">
-                          <label class="u-field-label" for="field-d13d">Item 2 ffffffffff ddddddddddddddddddddddddd</label>
-                        </div>
-                        <div class="u-input-row">
-                          <input id="field-e77f" type="checkbox" name="checkbox-1[]" value="Item 3 mmmm ddddddddddd ssssssssssss" class="u-field-input" data-calc="">
-                          <label class="u-field-label" for="field-e77f">Item 3 mmmm ddddddddddd ssssssssssss</label>
-                        </div>
-                        <div class="u-input-row">
-                          <input id="field-0695" type="checkbox" name="checkbox-1[]" value="item4" class="u-field-input" data-calc="">
-                          <label class="u-field-label" for="field-0695">item4</label>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="u-form-group u-form-partition-factor-2 u-form-select u-form-group-2">
-                      <label for="select-abbf" class="u-label">Como va a participar?</label>
-                      <div class="u-form-select-wrapper">
-                        <select id="select-abbf" name="select" class="u-border-2 u-border-custom-color-3 u-border-no-left u-border-no-right u-border-no-top u-custom-font u-font-montserrat u-input u-input-rectangle u-none u-input-1">
-                          <option value="Item 1" data-calc="">Item 1</option>
-                          <option value="Item 2" data-calc="">Item 2</option>
-                          <option value="Item 3" data-calc="">Item 3</option>
-                        </select>
-                        <svg class="u-caret u-caret-svg" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="16px" height="16px" viewBox="0 0 16 16" style="fill:currentColor;" xml:space="preserve"><polygon class="st0" points="8,12 2,4 14,4 "></polygon></svg>
-                      </div>
-                    </div>
-                    <div class="u-form-group u-form-partition-factor-2 u-form-select u-form-group-3">
-                      <label for="select-29f3" class="u-label">Provincia</label>
-                      <div class="u-form-select-wrapper">
-                        <select id="select-29f3" name="select-1" class="u-border-2 u-border-custom-color-3 u-border-no-left u-border-no-right u-border-no-top u-custom-font u-font-montserrat u-input u-input-rectangle u-none u-input-2">
-                          <option value="Item 1" data-calc="">Item 1</option>
-                          <option value="Item 2" data-calc="">Item 2</option>
-                          <option value="Item 3" data-calc="">Item 3</option>
-                        </select>
-                        <svg class="u-caret u-caret-svg" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="16px" height="16px" viewBox="0 0 16 16" style="fill:currentColor;" xml:space="preserve"><polygon class="st0" points="8,12 2,4 14,4 "></polygon></svg>
-                      </div>
-                    </div>
-                    <div class="u-form-group u-form-name u-form-partition-factor-2 u-form-group-4">
-                      <label for="name-8e54" class="u-label">Nombre de la Empresa:</label>
-                      <input type="text" id="name-8e54" name="name-2" class="u-border-2 u-border-custom-color-3 u-border-no-left u-border-no-right u-border-no-top u-custom-font u-font-montserrat u-input u-input-rectangle u-none u-input-3" required="">
-                    </div>
-                    <div class="u-form-email u-form-group u-form-partition-factor-2 u-form-group-5">
-                      <label for="email-c6a3" class="u-label">CUIT:</label>
-                      <input type="email" id="email-c6a3" name="email" class="u-border-2 u-border-custom-color-3 u-border-no-left u-border-no-right u-border-no-top u-custom-font u-font-montserrat u-input u-input-rectangle u-none u-input-4" required="">
-                    </div>
-                    <div class="u-form-group u-form-name u-form-partition-factor-3 u-form-group-6">
-                      <label for="name-8e54" class="u-label">Persona responsable:</label>
-                      <input type="text" id="name-8e54" name="name-1" class="u-border-2 u-border-custom-color-3 u-border-no-left u-border-no-right u-border-no-top u-custom-font u-font-montserrat u-input u-input-rectangle u-none u-input-5" required="">
-                    </div>
-                    <div class="u-form-group u-form-partition-factor-3 u-form-phone u-form-group-7">
-                      <label for="phone-84d9" class="u-label">Teléfono:</label>
-                      <input type="tel" pattern="\+?\d{0,3}[\s\(\-]?([0-9]{2,3})[\s\)\-]?([\s\-]?)([0-9]{3})[\s\-]?([0-9]{2})[\s\-]?([0-9]{2})" id="phone-84d9" name="phone" class="u-border-2 u-border-custom-color-3 u-border-no-left u-border-no-right u-border-no-top u-custom-font u-font-montserrat u-input u-input-rectangle u-none u-input-6" required="">
-                    </div>
-                    <div class="u-form-group u-form-partition-factor-3 u-form-group-8">
-                      <label for="text-de71" class="u-label">Email:</label>
-                      <input type="text" placeholder="" id="text-de71" name="text" class="u-border-2 u-border-custom-color-3 u-border-no-left u-border-no-right u-border-no-top u-custom-font u-font-montserrat u-input u-input-rectangle u-none u-input-7">
-                    </div>
-                    <div class="u-align-center u-form-group u-form-submit u-form-group-9">
-                      <a href="#" class="u-active-custom-color-4 u-border-none u-btn u-btn-round u-btn-submit u-button-style u-custom-color-3 u-custom-font u-font-montserrat u-hover-custom-color-4 u-radius u-btn-1">Enviar </a>
-                      <input type="submit" value="submit" class="u-form-control-hidden">
-                    </div>
-                    <div class="u-form-send-message u-form-send-success">Gracias! Tu mensaje ha sido enviado.</div>
-                    <div class="u-form-send-error u-form-send-message">No se puede enviar su mensaje. Por favor, corrija los errores y vuelva a intentarlo.</div>
-                    <input type="hidden" value="" name="recaptchaResponse">
-                    <input type="hidden" name="formServices" value="bd81a2eb-7b10-7091-0f17-69e434a8b6e5">
-                  </form>
-                </div>
+
+              <?php
+                switch($tipo){
+                  case 'RN':  include('./estructura/_form_evento_rn.php');  break;
+                  case 'RI':  include('./estructura/_form_evento_ri.php');  break;
+                  case 'C' :  include('./estructura/_form_evento_c.php');   break;
+                  case 'F' :  include('./estructura/_form_evento_f.php');   break;
+                }                
+              ?>
+
               </div>
             </div>
+
+
           </div>
         </div>
       </div>
     </section>
     
     
+
     
     <footer class="u-align-center u-clearfix u-container-align-center u-footer u-grey-80 u-valign-bottom-lg u-valign-bottom-md u-valign-bottom-sm u-valign-bottom-xs u-footer" id="footer"><div class="data-layout-selected u-clearfix u-expanded-width u-layout-wrap u-layout-wrap-1">
         <div class="u-layout">
@@ -188,7 +134,7 @@ box-shadow: 5px 5px 20px 0 rgba(0,0,0,0.4) !important
             </div>
             <div class="u-container-style u-layout-cell u-size-32 u-layout-cell-3">
               <div class="u-container-layout u-valign-middle-lg u-valign-middle-md u-valign-middle-sm u-valign-middle-xs u-container-layout-3">
-                <p class="u-align-left u-small-text u-text u-text-variant u-text-1"> Centro CívicoAvenida Libertador General San Martín 750 Oeste | C.P: 5400 | San Juan | Argentina Conmutador:&nbsp;(0264) 459 2201<br>5º piso Núcleo 4 - ingreso 3<br>Horarios de Atención de 7:30hs a 13:30hs
+                <p class="u-align-left u-small-text u-text u-text-variant u-text-1"> Centro Cívico Avenida Libertador General San Martín 750 Oeste | C.P: 5400 | San Juan | Argentina Conmutador:&nbsp;(0264) 459 2201<br>5º piso Núcleo 4 - ingreso 3<br>Horarios de Atención de 7:30hs a 13:30hs
                 </p>
               </div>
             </div>
