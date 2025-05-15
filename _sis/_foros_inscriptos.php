27dac1777e49f21bbbbda2c387ecb1ab3b160bb5
Miguel Angel <miguelangel_medina@yahoo.com.ar>
2025-05-15 10:11:12 -0300

    T-18: Foros - Inscriptos (refactor)
    - mejora en el mapa de sitio

--- _sis/_foros_inscriptos.php
+++ _sis/_foros_inscriptos.php
@@ -143,7 +143,7 @@ $(document).ready(function(){
 
                                         <div class="page-title"></div>
                                         
-                                        <nav class="breadcrumb-style-one" aria-label="breadcrumb">
+                                        <nav class="breadcrumb-style-five" aria-label="breadcrumb">
                                             <ol class="breadcrumb">
                                                 <li class="breadcrumb-item"><a href="principal.php" title="Dashboard"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg><span class="inner-text"></span></a></li>
                                                 <li class="breadcrumb-item">Foros</li>
