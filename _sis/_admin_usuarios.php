3b58ba4a67374e70a8b0e5abc2c99b6c6dabdaad
Miguel Medina <38297128+sahcmedina@users.noreply.github.com>
2025-06-18 07:49:49 -0300

    Update _admin_usuarios.php

--- _sis/_admin_usuarios.php
+++ _sis/_admin_usuarios.php
@@ -570,7 +570,7 @@ $(document).ready(function(){
                                             <ol class="breadcrumb">
                                             <li class="breadcrumb-item"><a href="principal.php" title="Dashboard"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg><span class="inner-text"></span></a></li>
                                                 <li class="breadcrumb-item"> Administración </li>
-                                                <li class="breadcrumb-item active" aria-current="page"> Usuarios 4</li>
+                                                <li class="breadcrumb-item active" aria-current="page"> Usuarios 5</li>
                                             </ol>
                                         </nav>
                         
