7239f089e2c3c925c29e30b600f57233b3e16b1f
rodoleon01 <38299250+rodoleon01@users.noreply.github.com>
2025-06-25 19:12:43 -0300

    T-25 Limpiar codigo
    Mover archivos que no corresponden al sistema.

--- _sis/funciones/ajax_mostrar_foto_contacto_ver.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-	$foto = $_POST['foto_'];
-	$url_ = './images/contacto/'.$foto;
-?>
-<div class="thumb"> 
-	<img id="foto" name="foto" src="<?php echo $url_; ?>" width="100" height="100" >
-</div>
