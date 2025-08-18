d13cfd1f6131580d6a4d440cba5025e20d9dbbf7
Miguel Medina <38297128+sahcmedina@users.noreply.github.com>
2025-08-18 13:21:19 -0300

    Update mod3_rn_agenda_param_ajax_validar_upd.php

--- _sis/funciones/mod3_rn_agenda_param_ajax_validar_upd.php
+++ _sis/funciones/mod3_rn_agenda_param_ajax_validar_upd.php
@@ -8,11 +8,15 @@ if (isset($_POST["x_dia"]))     { $x_dia= $_POST["x_dia"]; 	        } else { $x_
 
 // Control: Faltan datos?
 $c1= 'ok';	$er1= '';
-if($hs=='' OR $user=='' OR $duracion=='' OR $x_dia==''){	$c1= 'er';	$er1= 'Faltan datos. '; }
+if($hs=='' OR $user=='' OR $duracion=='' OR $x_dia==''){	$c1= 'er';	$er1= '- Faltan datos. '; }
+
+// Control: # reuniones >1?
+$c2= 'ok';	$er2= '';
+if($x_dia<1 OR $x_dia>15){	$c2= 'er';	$er2= '- Cant. x día debe estar entre 1 y 15. '; }
 
 // Validacion
-if($c1== 'er'){
-	echo "<center><label style='font-weight:bold;color:red;'> Errores: ".$er1." </label></center>"; 
+if($c1== 'er' OR $c2== 'er'){
+	echo "<center><label style='font-weight:bold;color:red;'> Errores: ".$er1.$er2." </label></center>"; 
 
 }else{
 	$_SESSION['var_hs']= $hs;	       $_SESSION['var_user']= $user;	     $_SESSION['var_duracion']= $duracion;	   	 $_SESSION['var_x_dia']= $x_dia;
